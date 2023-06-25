a0e9bf7a99fa9e79a5564a2fff7a89a03fb63893
VicktorCash <v1kkkaz@yandex.ru>
2023-06-25 13:33:12 +0300

    Изучил асимптотику алгоритмов

---
+++ lesson_1.py
@@ -0,0 +1,29 @@
+a = "abcdfaa"
+
+#def strcounter(s):
+#    counter = 0
+ #   for sym in s:
+  #      counter += 1
+  #  print(counter)
+#strcounter(a)
+
+#def strcounter(s):
+    #for sym in set(s):
+      #  counter = 0
+   #     for subsym in s:
+    #        if sym == subsym:
+    #            counter += 1
+    #    print(sym, counter)
+#strcounter(a)
+
+def strcounter(s):
+
+    sym_counter = {}
+    for sym in s:
+        if sym not in sym_counter:
+            sym_counter[sym] = 1
+        else:
+            sym_counter[sym] +=1
+    for sym, count in sym_counter.items():
+        print(sym,count)
+strcounter(a)
