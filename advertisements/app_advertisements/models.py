89c87bb41db68422da8dddf0b5f0fa00c0006ec1
VicktorCash <v1kkkaz@yandex.ru>
2023-08-15 14:50:12 +0300

    Домашка к 8 занятию

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 
 class Advertisement(models.Model):
 
@@ -24,6 +25,30 @@ class Advertisement(models.Model):
     #Уместен ли торг
     auction = models.BooleanField("Торг", help_text="Отметьте, если хотите торговаться")
     
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.strftime("%H:%M:%S")
+            return format_html("<span style='color:green; font-weight:bold'> Сегодня в {}</span>", created_time)
+
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+    @admin.display(description="Дата обновления")
+    def updated_date(self):
+        from django.utils import timezone
+
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.strftime("%H:%M:%S")
+            return format_html("<span style='color:green; font-weight:bold'> Сегодня в {}</span>", updated_time)
+
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+
+  
+
+
     def __str__(self):
         return f"Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price}"
 
